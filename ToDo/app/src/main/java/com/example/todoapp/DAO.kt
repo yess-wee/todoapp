5e2178d94297f76de8cf38ae1a6a8a4220d0f1de
Yashvi Chauhan <yashvichauhan83@gmail.com>
2024-05-11 00:04:45 +0530

    room database init

--- ToDo/app/src/main/java/com/example/todoapp/DAO.kt
+++ ToDo/app/src/main/java/com/example/todoapp/DAO.kt
@@ -1,22 +1,22 @@
 package com.example.todoapp
 
-import androidx.room.Dao
-import androidx.room.Delete
-import androidx.room.Insert
-import androidx.room.Query
-import androidx.room.Update
+import androidx.room.*
 
 @Dao
 interface DAO {
     @Insert
-    fun insertTask(entity: Entity)
+    suspend fun insertTask(entity: Entity)
 
     @Update
-    fun updateTask(entity: Entity)
+    suspend fun updateTask(entity: Entity)
 
     @Delete
-    fun deleteTask(entity: Entity)
+    suspend fun deleteTask(entity: Entity)
 
     @Query("Delete from to_do")
-    fun deleteAll()
+    suspend fun deleteAll()
+
+    @Query("Select * from to_do")
+    suspend fun getTasks():List<CardInfo>
+
 }
